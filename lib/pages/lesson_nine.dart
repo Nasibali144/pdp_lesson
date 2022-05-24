88d5560dd428ad6aab4084eb68e2dbfdf9ddeb9d
nasibali <abdiyevnasibali@gmail.com>
2022-05-24 17:22:38 +0500

    Flutter B17 commit 2

--- lib/pages/lesson_nine.dart
+++ lib/pages/lesson_nine.dart
@@ -9,9 +9,14 @@ class LessonNine extends StatefulWidget {
 }
 
 class _LessonNineState extends State<LessonNine> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    print("Height: ${MediaQuery.of(context).size.height}");
+    print("Width: ${MediaQuery.of(context).size.width}");
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -29,7 +34,66 @@ class _LessonNineState extends State<LessonNine> {
         ],
       ),
       body: Container(
-
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          color: Colors.white
+          // image: DecorationImage(
+          //   fit: BoxFit.cover,
+          //   image: AssetImage("assets/images/background.png")
+          // )
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.white,
+        child: Icon(Icons.home, size: 35, color: Colors.black,),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        landscapeLayout: BottomNavigationBarLandscapeLayout.linear,
+        backgroundColor: Colors.deepPurple,
+        selectedFontSize: 16,
+        unselectedFontSize: 14,
+        selectedIconTheme: IconThemeData(size: 27.5),
+        unselectedIconTheme: IconThemeData(size: 24),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey.shade300,
+        showUnselectedLabels: false,
+        currentIndex: _currentIndex,
+        enableFeedback: true,
+        // fixedColor: Colors.red,
+        // elevation: 0,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: "Home",
+            activeIcon: Icon(Icons.home),
+            // tooltip: "Name"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline),
+            label: "Search",
+            activeIcon: Icon(Icons.favorite),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_outline),
+            label: "Search",
+            activeIcon: Icon(Icons.bookmark),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: "Search",
+            activeIcon: Icon(Icons.account_circle),
+          ),
+        ],
       ),
     );
   }
